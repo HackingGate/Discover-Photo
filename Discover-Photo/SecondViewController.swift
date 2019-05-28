d6251296724fcd23a78ad79f94a3f61a0d38fc69
HackingGate <i@hackinggate.com>
2019-05-28 22:02:13 +0900

    Create iOS project

---
+++ Discover-Photo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Discover-Photo
+//
+//  Created by ERU on 2019/05/28.
+//  Copyright © 2019 HackingGate. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
