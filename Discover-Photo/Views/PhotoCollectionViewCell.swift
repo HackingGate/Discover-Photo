aad3b28bb747c2f285bffbbb38b5e4e2b0957ef9
HackingGate <i@hackinggate.com>
2019-06-03 22:29:37 +0900

    Add label and gradient layer to cell

--- Discover-Photo/Views/PhotoCollectionViewCell.swift
+++ Discover-Photo/Views/PhotoCollectionViewCell.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     
     static let reuseIdentifier = "PhotoCell"
 
     @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var textLabel: UILabel!
     
+    func configure(photo: UnsplashPhoto, cornerRadius: CGFloat) {
+        photoView.layer.cornerRadius = cornerRadius
+        if let hexColorString = photo.color {
+            photoView.backgroundColor = UIColor.hexStringToUIColor(hex: hexColorString)
+        }
+        Alamofire.request(photo.urls.thumb).responseImage { response in
+            if let image = response.result.value {
+                let roundedImage = image.af_imageScaled(to: self.photoView.bounds.size).af_imageRounded(withCornerRadius: cornerRadius)
+                let gradientLayer = CAGradientLayer()
+                gradientLayer.frame = self.blurView.bounds
+                gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+                gradientLayer.locations = [0.6180339887, 1.0] // Golden ratio
+                gradientLayer.cornerRadius = cornerRadius
+                self.blurView.layer.mask = gradientLayer
+                self.photoView.image = roundedImage
+            }
+        }
+        textLabel.text = photo.user.username
+    }
 }
